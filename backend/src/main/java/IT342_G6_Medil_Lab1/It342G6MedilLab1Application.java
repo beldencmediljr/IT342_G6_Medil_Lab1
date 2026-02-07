2260928fabfb398e988f9b62ae009367f38cf3fe
LAB13_USER ON WS23 <L13Y17W23@LabHCI.net>
2026-02-07 16:43:35 +0800

    Initial Lab 1 submission

---
+++ backend/src/main/java/IT342_G6_Medil_Lab1/It342G6MedilLab1Application.java
@@ -0,0 +1,13 @@
+package IT342_G6_Medil_Lab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class It342G6MedilLab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(It342G6MedilLab1Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/IT342_G6_Medil_Lab1/It342G6MedilLab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package IT342_G6_Medil_Lab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class It342G6MedilLab1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
